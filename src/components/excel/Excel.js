d2996639aea8daae452284f05879b2f2d16b82de
AbramovichCp <abramovich.cp@gmail.com>
2020-05-22 10:34:09 +0300

    add tamplete and polifill

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,19 +1,23 @@
+import { $ } from '@core/Dom'
+
 export class Excel {
   constructor(selector, options) {
-    this.$el = document.querySelector(selector)
+    this.$el = $(selector)
     this.components = options.components || []
   }
 
   getRoot() {
-    const $root = document.createElement('div')
+    const $root = $.create('div', 'excel')
     this.components.forEach(Component => {
-      const component = new Component()
-      $root.insertAdjacentHTML('beforeend', component.toHTML())
+      const $el = $.create('div', Component.className)
+      const component = new Component($el)
+      $el.html(component.toHTML())
+      $root.append($el)
     })
 
     return $root
   }
   render() {
-    this.$el.append( this.getRoot())
+    this.$el.append(this.getRoot())
   }
 }
--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,1 +1,8 @@
-export class DomListener {}
+export class DomListener {
+  constructor($root) {
+    if (!$root) {
+      throw new Error('No $root provided for DOM listener')
+    }
+    this.$root = $root
+  }
+}
